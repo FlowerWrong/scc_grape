4a58ee33a415cbf1e80c4f44a2026fe16e620b02
FlowerWrong <sysuyangkang@gmail.com>
2019-03-29 15:42:47 +0800

    fix typo

--- lib/scc_grape/api.rb
+++ lib/scc_grape/api.rb
@@ -19,9 +19,9 @@ module SccGrape
 
     def self.reload_config
       if ENV['CONFIG_SERVER_URL'].blank?
-        puts 'No CONFIG_SERVER_URL provided, scc_rails will not fetch config from spring cloud config server'
+        puts 'No CONFIG_SERVER_URL provided, scc_grape will not fetch config from spring cloud config server'
       elsif ENV['APP_NAME'].blank?
-        puts 'No APP_NAME provided, scc_rails will not fetch config from spring cloud config server'
+        puts 'No APP_NAME provided, scc_grape will not fetch config from spring cloud config server'
       else
         $cloud_env = SccRuby::Api.fetch(ENV['CONFIG_SERVER_URL'], ENV['APP_NAME'], ENV['APP_ENV'] || 'default', ENV['CLOUD_USERNAME'] || '', ENV['CLOUD_PASSWORD'] || '')
       end
